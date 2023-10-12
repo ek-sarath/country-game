1132a0506a1f575cba4a93eae2be746186d79041
ek-sarath <sarath.e@edstem.com>
2023-10-12 10:23:56 +0530

    Ongoing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
 import './App.css';
+import CountryCapital from './components/CountryCapital'
 
 function App() {
   return (
     <div className="App">
-      
+      <CountryCapital data={data}/>
     </div>
   );
 }
---
+++ src/components/CountryCapital.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+
+const data = {
+    India: "New Delhi",
+    Australia: "Canberra",
+    Germany: "Berlin",
+    Poland: "Warszawa",
+};
+
+const CountryCapital = () => {
+};
+
+ return (
+    <div className='App'>
+    <h2>Country Capital Game</h2>
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+};
+
+export default CountryCapital;
+
+ 

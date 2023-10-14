c3bf9f7d91fab2137e7e23fcd2091155df1e7279
ek-sarath <sarath.e@edstem.com>
2023-10-14 17:57:48 +0530

    CountryCapital

--- src/components/CountryCapital.js
+++ src/components/CountryCapital.js
@@ -1,19 +1,37 @@
+import React, {useState} from "react";
+export default CountryCapital;
+
 function CountryCapital ({data}) {
+    const [selectedAnswer, setSelectedAnswer] = useState();
+    const [prevSelectedAnswer, setPrevSelectedAnswer] = useState();
+    const [buttonList, setButtonList] = useState(shuffle([...Object.keys(data), ...Object.values (data)]));
+
 
     function shuffle (countries) {
     
-    for (let i : number = countries.length-1; i>0; i--) {
-    
-    const j : number = Math.floor(Math.random() * (i+ 1));
-    
-    [countries[i], countries[j]] = [countries[j], countries[i]];
+    for (let i = countries.length-1; i>0; i--) {
+         const j = Math.floor(Math.random() * (i+ 1));
+         [countries[i], countries[j]] = [countries[j], countries[i]];
     
     }
     
     return countries;
 }
+  
 
-    const buttonList = shuffle( countries: [...Object.keys(data), ...Object.values (data)});
+   const handleAnswer = (e) => {
+    const answer = e.target.value
+    if (!selectedAnswer){
+        setSelectedAnswer(answer)
+    }else{
+        if(data[setSelectedAnswer] === answer || data[answer] === selectedAnswer){
+            setButtonList(buttonList.filter(b=>b!==answer&&b!== selectedAnswer));
+            setSelectedAnswer=(null)
+    }
+   }
+
+
+    const buttonList = shuffle([...Object.keys(data), ...Object.values (data)]);
 
     
     console.log(buttonList);
@@ -28,87 +46,4 @@ function CountryCapital ({data}) {
 </>;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, {useState} from "react";
-
-// const CountryCapital = ({data}) => {
-//   const [entries, setEntries] = useState(() => {
-//     return Object.entries(data).sort(() => Math.random() - 0.5);
-//   });
-
-//   const [state, setState] = useState({
-//     country: "",
-//     capital: "",
-//     correct: false,
-//   });
-
-//   const checkAnswer = (country, capital) => {
-//     if (data[country] === capital) {
-//       const filteredEntries = entries.filter(
-//         ([c, cap]) => !(c === country && cap === capital)
-//       );
-//       setEntries(filteredEntries);
-//       setState({ country: "", capital: "", correct: true });
-//     } else {
-//       setState({ country, capital, correct: false });
-//     }
-//   };
-
-
-//   return (
-//     <div className="countryCapital">
-//       <h1>Country to Capital Matching Game</h1>
-//       <p>NB: You need to match a country to its capital by clicking on appropriate buttons</p>
-
-//       {entries.length > 0 ? (
-//         <div className="buttonContainer">
-//           {entries.map(([country, capital]) => (
-//             <div key={country}>
-//               <button
-//                 style={{
-//                   backgroundColor: state.country === country ? "#0000ff" : "",
-//                 }}
-//                 onClick={() => checkAnswer(country, state.capital)}
-//               >
-//                 {country}
-//               </button>
-//               <button
-//                 style={{
-//                   backgroundColor: state.capital === capital ? "#0000ff" : "",
-//                 }}
-//                 onClick={() => checkAnswer(state.country, capital)}
-//               >
-//                 {capital}
-//               </button>
-//             </div>
-//           ))}
-//         </div>
-//       ) : (
-//         <h3>Congratulations You Won The Game!!!</h3>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default CountryCapital;
+export default CountryCapital;

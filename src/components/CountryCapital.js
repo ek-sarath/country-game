95c9732913c0d481524c5b1c66ac6071e9b90a4d
ek-sarath <sarath.e@edstem.com>
2023-10-14 18:14:12 +0530

    Progressing

--- src/components/CountryCapital.js
+++ src/components/CountryCapital.js
@@ -1,42 +1,41 @@
-import React, {useState} from "react";
-export default CountryCapital;
+import {useState} from "react";
 
 function CountryCapital ({data}) {
-    const [selectedAnswer, setSelectedAnswer] = useState();
-    const [prevSelectedAnswer, setPrevSelectedAnswer] = useState();
+    const [selectedAnswer, setSelectedAnswer] = useState(null);
+    const [prevSelectedAnswer, setPrevSelectedAnswer] = useState(null);
     const [buttonList, setButtonList] = useState(shuffle([...Object.keys(data), ...Object.values (data)]));
 
-
     function shuffle (countries) {
-    
-    for (let i = countries.length-1; i>0; i--) {
-         const j = Math.floor(Math.random() * (i+ 1));
-         [countries[i], countries[j]] = [countries[j], countries[i]];
-    
+       for (let i = countries.length-1; i>0; i--) {
+           const j = Math.floor(Math.random() * (i+ 1));
+           [countries[i], countries[j]] = [countries[j], countries[i]];
     }
-    
     return countries;
 }
   
-
    const handleAnswer = (e) => {
-    const answer = e.target.value
+    const answer = e.target.value;
     if (!selectedAnswer){
-        setSelectedAnswer(answer)
+        setSelectedAnswer(answer);
+
     }else{
         if(data[setSelectedAnswer] === answer || data[answer] === selectedAnswer){
             setButtonList(buttonList.filter(b=>b!==answer&&b!== selectedAnswer));
-            setSelectedAnswer=(null)
-    }
-   }
+            setSelectedAnswer=(null);
+            setPrevSelectedAnswer=(null);
+    }else{
+        setPrevSelectedAnswer(selectedAnswer);
+        setSelectedAnswer(answer);
 
 
-    const buttonList = shuffle([...Object.keys(data), ...Object.values (data)]);
 
-    
-    console.log(buttonList);
-    
-    
+
+
+
+      }
+     }
+   };
+
     return <>
     {
     buttonList.map((item) => { 

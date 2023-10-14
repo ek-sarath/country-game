1ef2927403d18840427ee92ad1188dd975ea9dd7
ek-sarath <sarath.e@edstem.com>
2023-10-14 19:02:21 +0530

    Country Capital

--- src/components/CountryCapital.js
+++ src/components/CountryCapital.js
@@ -4,6 +4,7 @@ function CountryCapital ({data}) {
     const [selectedAnswer, setSelectedAnswer] = useState(null);
     const [prevSelectedAnswer, setPrevSelectedAnswer] = useState(null);
     const [buttonList, setButtonList] = useState(shuffle([...Object.keys(data), ...Object.values (data)]));
+    const [color, setColor] = useState('');
 
     function shuffle (countries) {
        for (let i = countries.length-1; i>0; i--) {
@@ -17,7 +18,7 @@ function CountryCapital ({data}) {
     const answer = e.target.value;
     if (!selectedAnswer){
         setSelectedAnswer(answer);
-
+        setColor ('blue');
     }else{
         if(data[setSelectedAnswer] === answer || data[answer] === selectedAnswer){
             setButtonList(buttonList.filter(b=>b!==answer&&b!== selectedAnswer));
@@ -36,10 +37,17 @@ function CountryCapital ({data}) {
      }
    };
 
+   if (buttonList.length === 0){
+     return <p>Congratulations</p>
+   }
+
     return <>
     {
     buttonList.map((item) => { 
-    return <button key = {item} className="game-btn">{item}</button>
+    return <button key = {item} className={`game-btn ${selectedAnswer === item? "selected" : ""}
+    ${prevSelectedAnswer && (item === selectedAnswer || item === prevSelectedAnswer) ?"Incorrect" :""}`}
+    style={{backgroundColor: `${color}`}}
+    onClick={handleAnswer} value={item}>{item}</button>
     })
  }
 </>;
